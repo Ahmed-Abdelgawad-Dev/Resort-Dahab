27d44c0fe9de733c3ede3443d2381908eb0f095c
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2022-01-23 04:22:20 +0200

    initial nextjs code commit

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+
+export default function about() {
+  return (
+    <div>
+      <Head>
+        <title>about page</title>
+      </Head>
+      <main>
+          <div>ABOUT</div>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Blog</title>
+        <meta name="Blog with next js" content="My Blog" />
+      </Head>
+      <main>
+          main
+      </main>
+
+      <footer>
+        Footer
+      </footer>
+    </div>
+  )
+}
